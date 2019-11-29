8cc75686e2dea0ad562a380fa51e2e1c7b6db426
ubergesundheit <ubergesundheit@googlemail.com>
2019-11-29 23:29:24 +0100

    Implement javascript mail address reversal

--- hugo/themes/piercer/assets/js/main.js
+++ hugo/themes/piercer/assets/js/main.js
@@ -1,6 +1,23 @@
 // Header
-const header = document.querySelector('header');
+const header = document.querySelector("header");
 
 window.addEventListener("load", () => {
-    document.body.style.paddingTop = `${header.offsetHeight}px`;
+  document.body.style.paddingTop = `${header.offsetHeight}px`;
+
+  const anchors = document.querySelectorAll("a");
+  for (const anchor of anchors) {
+    if (anchor.href.startsWith("mailto:")) {
+      const [address, ...rest] = anchor.href.slice(7).split("?");
+      const reversed = address
+        .split("")
+        .reverse()
+        .join("");
+      anchor.href = `mailto:${reversed}${
+        rest.length !== 0 ? `?${rest.join("")}` : ""
+      }`;
+      if (anchor.textContent.trim() === address) {
+        anchor.textContent = reversed;
+      }
+    }
+  }
 });
