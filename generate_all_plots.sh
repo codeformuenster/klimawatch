999d094aa0fa8027b9af34098108ecaf32690184
Thomas Kluth <t.kluth@posteo.de>
2020-03-17 22:48:05 +0100

    fix trend-hack for karlsruhe

--- generate_all_plots.sh
+++ generate_all_plots.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 for city in muenster hamburg karlsruhe koeln landau leipzig
 do
-    python generate_plots.py $city
+    if test "$city" == "karlsruhe"
+    then
+        python generate_plots.py $city 2007
+    else
+        python generate_plots.py $city
+    fi
 done
